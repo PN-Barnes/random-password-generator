a3ff64fcc6d64c6222ea51242ced889f990eabaa
PN-Barnes <paulieb99@gmail.com>
2021-06-23 14:20:55 -0700

    Created arrays for characters within script.js

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,12 @@ if(passwordLength.length < 8 || passwordLength.length > 128 ) {
   alert('Your password length must be between 8 and 128 characters.')
 }
 
+var upperAlphabetChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+var lowerAlphabetChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 var confirmLowercase = confirm('Do you want Lowercase letters in your password?');
 
 var confirmUppercase = confirm('Do you want Uppercase letters in your password?');
