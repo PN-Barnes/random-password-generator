92456f93f9b08717f5175b8e69c8e4f549927a09
PN-Barnes <paulieb99@gmail.com>
2021-06-23 13:14:49 -0700

    Added confirm prompts to variables to start writing functionality

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passwordLength = prompt('How long do you want your password?')
+
+if(passwordLength.length < 8 || passwordLength.length > 128 ) {
+  alert('Your password length must be between 8 and 128 characters.')
+}
+
+var confirmLowercase = confirm('Do you want Lowercase letters in your password?');
+
+var confirmUppercase = confirm('Do you want Uppercase letters in your password?');
+
+var confirmNumbers = confirm('Do you want Numbers in your password?');
+
+var confirmSpecialCharacters = confirm('Do you want any special characters in your password?')
