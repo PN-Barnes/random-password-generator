3046f3c011c5f5b8ae6526e965d925d4141b46f5
PN-Barnes <paulieb99@gmail.com>
2021-06-24 17:52:56 -0700

    Code finally accepts specific parameters and generates password with criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 // ---------------------------------------------------------------------------------------------
+var arrayCharsArr =[['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']]
 var charactersArr = []
 var upperAlphabetChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lowerAlphabetChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -36,23 +37,70 @@ function writePassword() {
   var confirmSpecialCharacters = confirm('Do you want any special characters in your password?')
   var checkConfirmsArr = [confirmLowercase, confirmUppercase, confirmNumbers, confirmSpecialCharacters]
 
-  function sort() {
-    for ( i = 0; i < checkConfirmsArr.length; i++) {
-      if(checkConfirmsArr[i] === false) {
-        deniedCharacters.push(checkConfirmsArr[i])
-      } else {
-        charactersArr.push(checkConfirmsArr[i])
-      }
-    }
-    return charactersArr;
-  }
-  
-  console.log(checkConfirmsArr.length)
+    // for ( i = 0; i < checkConfirmsArr.length; i++) {
+    //   if(confirmLowercase === false) {
+    //     deniedCharacters.push(arrayCharsArr[i])
+    //   } else {
+    //     charactersArr.push(arrayCharsArr[i])
+    //   }
+    //   return
+    // }
+
+    if (confirmLowercase) {
+      charactersArr.push(lowerAlphabetChars)
+      console.log(charactersArr);
+    } 
+
+    if (confirmUppercase) {
+      charactersArr.push(upperAlphabetChars);
+      console.log(charactersArr)
+    } 
+
+    if(confirmNumbers) {
+      charactersArr.push(numbersArr);
+      console.log(charactersArr)
+    } 
+
+    if(confirmSpecialCharacters) {
+      charactersArr.push(specialChars);
+      console.log(charactersArr)
+    } 
+    if (confirmSpecialCharacters === false && confirmUppercase === false && confirmLowercase === false && confirmNumbers === false) {
+      alert("You must select at least one parameter to generate a password. Refresh the page and try again.");
+    } 
+    
+    console.log(charactersArr)
+
+  // if (confirmLowercase && confirmUppercase && confirmNumbers && confirmSpecialCharacters) {
+  //   charactersArr.push(upperAlphabetChars);
+  //   charactersArr.push(lowerAlphabetChars);
+  //   charactersArr.push(numbersArr);
+  //   charactersArr.push(specialChars);
+  // } else if(confirmLowercase === false) {
+  //   charactersArr.push(upperAlphabetChars);
+  //   charactersArr.push(numbersArr);
+  //   charactersArr.push(specialChars);
+  // } else if(confirmUppercase === false) {
+  //   charactersArr.push(lowerAlphabetChars);
+  //   charactersArr.push(numbersArr);
+  //   charactersArr.push(specialChars);
+  // } else if(confirmNumbers === false) {
+  //   charactersArr.push(upperAlphabetChars);
+  //   charactersArr.push(lowerAlphabetChars);
+  //   charactersArr.push(specialChars);
+  // } else if(confirmSpecialCharacters === false) {
+  //   charactersArr.push(upperAlphabetChars);
+  //   charactersArr.push(lowerAlphabetChars);
+  //   charactersArr.push(numbersArr);
+  // }else {
+  //   return alert("You must select at least one parameter to generate the password.");
+  // }
   //var password = generatePassword();
   for (i=0; i < passwordLength; i++) {
     var randomIndex = Math.floor(Math.random() * charactersArr.length);
     var randomSelect = Math.floor(Math.random() * charactersArr[randomIndex].length);
     var randomSelectedCharacter = charactersArr[randomIndex][randomSelect];
+    console.log(randomSelectedCharacter)
     passwordArrayCharacters.push(randomSelectedCharacter);
     generatedPassword = passwordArrayCharacters.join('')
   }
@@ -60,9 +108,11 @@ function writePassword() {
 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = generatePassword;
+  passwordText.value = generatedPassword;
     
-} // End of function writePassword()
+} 
+
+// End of function writePassword()
 
 
 
